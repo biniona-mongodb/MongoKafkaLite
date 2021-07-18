8de66419b519f58b7141b0d97afe08aa7427d2ef
Aleksander Binion <alek.binion@mongodb.com>
2021-07-18 17:30:25 -0400

    exponential backoff to test for kafka connect ready

--- initialize-container.sh
+++ initialize-container.sh
@@ -1,13 +1,12 @@
-mongo config-replica.js
-# config-replica.js must complete before collections can be built. Need to confirm this
-sleep 8
-# build collections. I think this needs to happen before connectors can be added. Need to confirm this
-mongo config-data.js
-sleep 8
-# delete connectors and re add them.
-curl -X DELETE -H "Content-Type: application/json" http://connect:8083/connectors/mongo-source
-curl -X DELETE -H "Content-Type: application/json" http://connect:8083/connectors/mongo-sink
-curl -X POST -H "Content-Type: application/json" --data @source-connector.json http://connect:8083/connectors -w "\n"
+#!/bin/sh
+# exponential back off as kafka connect starts
+curl --connect-timeout 5 \
+     --max-time 10 \
+     --retry 6 \
+     --retry-delay 0 \
+     --retry-max-time 80 \
+     --retry-connrefused \
+     -X POST -H "Content-Type: application/json" --data @source-connector.json http://connect:8083/connectors -w "\n"
 curl -X POST -H "Content-Type: application/json" --data @sink-connector.json http://connect:8083/connectors -w "\n"
 # print all connectors added to kafka connect
 curl -X GET http://connect:8083/connectors
