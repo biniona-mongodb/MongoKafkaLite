c44991cd2a3044e69855711eeffe1182b69b708b
Aleksander Binion <alek.binion@mongodb.com>
2021-07-17 22:24:33 -0400

    increase sleep time

--- initialize-container.sh
+++ initialize-container.sh
@@ -1,9 +1,9 @@
 mongo config-replica.js
 # config-replica.js must complete before collections can be built. Need to confirm this
-sleep 3
+sleep 8
 # build collections. I think this needs to happen before connectors can be added. Need to confirm this
 mongo config-data.js
-sleep 3
+sleep 8
 # delete connectors and re add them.
 curl -X DELETE -H "Content-Type: application/json" http://connect:8083/connectors/mongo-source
 curl -X DELETE -H "Content-Type: application/json" http://connect:8083/connectors/mongo-sink
