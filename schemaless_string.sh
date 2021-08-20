63324226cfa3a2d06da8f9db08580ee366d44991
Aleksander Binion <alek.binion@mongodb.com>
2021-08-20 12:49:40 -0400

    add schemaless json and string configs

---
+++ schemaless_string.sh
@@ -0,0 +1,37 @@
+curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '
+     {"name": "mongo-source",
+      "config": {
+         "connector.class":"com.mongodb.kafka.connect.MongoSourceConnector",
+         "connection.uri":"mongodb://mongo1:27017/?replicaSet=rs0",
+         "database":"quickstart",
+         "collection":"source",
+         "output.format.value":"json",
+         "output.format.key":"json",
+         "key.converter.schemas.enable":false,
+         "value.converter.schemas.enable":false,
+         "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+         "value.converter":"org.apache.kafka.connect.storage.StringConverter"
+         }
+     }
+     ' \
+     http://connect:8083/connectors -w "\n"
+
+curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '
+     {"name": "mongo-sink",
+      "config": {
+         "connector.class":"com.mongodb.kafka.connect.MongoSinkConnector",
+         "connection.uri":"mongodb://mongo1:27017/?replicaSet=rs0",
+         "database":"quickstart",
+         "collection":"sink",
+         "topics":"quickstart.source",
+         "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+         "value.converter":"org.apache.kafka.connect.storage.StringConverter"
+         }
+     }
+     ' \
+     http://connect:8083/connectors -w "\n"
+
