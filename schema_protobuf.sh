ac1e83ed4dba79e30b0615e9bea4d7a54d1d5050
Aleksander Binion <alek.binion@mongodb.com>
2021-08-20 09:56:32 -0400

    protobuf test

---
+++ schema_protobuf.sh
@@ -0,0 +1,38 @@
+curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '
+     {"name": "mongo-source",
+      "config": {
+         "connector.class":"com.mongodb.kafka.connect.MongoSourceConnector",
+         "connection.uri":"mongodb://mongo1:27017/?replicaSet=rs0",
+         "database":"quickstart",
+         "collection":"source",
+         "output.format.value":"schema",
+         "output.format.key":"schema",
+         "key.converter":"io.confluent.connect.protobuf.ProtobufConverter",
+         "key.converter.schema.registry.url":"http://schema-registry:8081",
+         "value.converter":"io.confluent.connect.protobuf.ProtobufConverter",
+         "value.converter.schema.registry.url":"http://schema-registry:8081"
+         }
+     }
+     ' \
+     http://connect:8083/connectors -w "\n"
+
+curl -X POST \
+     -H "Content-Type: application/json" \
+     --data '
+     {"name": "mongo-sink",
+      "config": {
+         "connector.class":"com.mongodb.kafka.connect.MongoSinkConnector",
+         "connection.uri":"mongodb://mongo1:27017/?replicaSet=rs0",
+         "database":"quickstart",
+         "collection":"sink",
+         "topics":"quickstart.source",
+         "key.converter":"io.confluent.connect.protobuf.ProtobufConverter",
+         "key.converter.schema.registry.url":"http://schema-registry:8081",
+         "value.converter":"io.confluent.connect.protobuf.ProtobufConverter",
+         "value.converter.schema.registry.url":"http://schema-registry:8081"
+         }
+     }
+     ' \
+     http://connect:8083/connectors -w "\n"
